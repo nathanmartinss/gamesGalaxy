fc12989e1c5cc85aacb2ddd3a298ac012571b84e
Nathan <naa.limaa@hotmail.com>
2024-11-08 14:15:39 -0300

    fix Files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./components/ItemListContainer/ItemListContainer.css";
+
+function App() {
+  return (
+    <CartProvider>
+      <div className="App">
+        <NavBar />
+        <div className="itens-mais-vendidos">
+          <ItemListContainer greeting="MAIS VENDIDOS!" />
+        </div>
+        <ItemDetailContainer />
+      </div>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Item/ItemList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Item from "../Item/Item";
+
+const ItemList = ({ items }) => {
+  return (
+    <div className="d-flex justify-content-around flex-wrap">
+      {items.map((item) => (
+        <Item
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          description={item.description}
+          price={item.price}
+          pictureUrl={item.pictureUrl}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ItemList;

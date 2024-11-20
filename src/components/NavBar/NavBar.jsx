b9e2d9db937ca2f42cadda6918e9e5c8949e6e48
nathan <nathanmartinss@icloud.com>
2024-11-20 20:25:52 -0300

    fix NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,16 +14,19 @@ const NavBar = () => {
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mx-auto">
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link" to="/">
                 Início
@@ -40,13 +43,8 @@ const NavBar = () => {
               </Link>
             </li>
           </ul>
-          <ul className="navbar-nav ml-auto nav-item-login">
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-          </ul>
+        </div>
+        <div className="cart-icon-container">
           <CartWidget />
         </div>
       </div>

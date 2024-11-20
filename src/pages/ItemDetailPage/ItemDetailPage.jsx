8d64d3f7a8ee890e3be1d943b4eacb7f567a0a76
nathan <nathanmartinss@icloud.com>
2024-11-20 18:30:56 -0300

    fix HomePage

--- src/pages/ItemDetailPage/ItemDetailPage.jsx
+++ src/pages/ItemDetailPage/ItemDetailPage.jsx
@@ -13,6 +13,24 @@ const itemData = {
       "https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$",
     stock: 5,
   },
+  2: {
+    title: "Xbox Series X",
+    description:
+      "O console mais poderoso da Microsoft, com gráficos incríveis e desempenho ultrarrápido.",
+    price: 4599,
+    pictureUrl:
+      "https://cms-assets.xboxservices.com/assets/68/a0/68a0e50d-0d13-42b1-8498-e55cef8a9133.png?n=642227_Hero-Gallery-0_A2_857x676.png",
+    stock: 5,
+  },
+  3: {
+    title: "Nintendo Switch",
+    description:
+      "A versatilidade de um console híbrido: jogue na TV ou em qualquer lugar.",
+    price: 2999,
+    pictureUrl:
+      "https://assets.nintendo.com/image/upload/f_auto/q_auto/c_fill,w_300/ncom/en_US/switch/system/three-modes-in-one",
+    stock: 5,
+  },
 };
 
 const ItemDetailPage = () => {
@@ -25,6 +43,19 @@ const ItemDetailPage = () => {
     alert(`Adicionado ${quantity} unidades de ${item.title} ao carrinho!`);
   };
 
+  if (!item) {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginTop: "50px",
+        }}
+      >
+        <h2>Produto não encontrado</h2>
+      </div>
+    );
+  }
+
   return (
     <div
       className="container"
@@ -36,11 +67,7 @@ const ItemDetailPage = () => {
           <h5 className="card-title">{item.title}</h5>
           <p className="card-text">{item.description}</p>
           <p className="card-text">Preço: R$ {item.price}</p>
-          <ItemCount
-            stock={item.stock}
-            initial={1}
-            onAdd={handleAddToCart}
-          />{" "}
+          <ItemCount stock={item.stock} initial={1} onAdd={handleAddToCart} />
         </div>
       </div>
     </div>

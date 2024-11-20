a76ad10eee402f795cec3f4c878ad6920b114024
nathan <nathanmartinss@icloud.com>
2024-11-20 20:51:06 -0300

    fix ItemDetail

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useCart } from "../../context/CartContext";
 import ItemCount from "../ItemCount/ItemCount";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const ItemDetail = ({ id, title, description, price, pictureUrl, stock }) => {
-  const [addedQuantity, setAddedQuantity] = useState(0);
+  const { addItem, getItemQuantity } = useCart();
   const navigate = useNavigate();
+  const [addedQuantity, setAddedQuantity] = useState(0);
 
   const handleAddToCart = (quantity) => {
+    const item = { id, title, price, stock, pictureUrl };
+    addItem(item, quantity);
     setAddedQuantity(quantity);
   };
 
   const handleFinalizePurchase = () => {
     navigate("/cart");
   };
+
+  const currentQuantityInCart = getItemQuantity(id);
+  const maxStockAvailable = stock - currentQuantityInCart;
 
   return (
     <div className="card" style={{ width: "18rem", margin: "20px auto" }}>
@@ -23,11 +30,15 @@ const ItemDetail = ({ id, title, description, price, pictureUrl, stock }) => {
         <p className="card-text">{description}</p>
         <p className="card-text">Preço: R$ {price}</p>
         {addedQuantity === 0 ? (
-          <ItemCount stock={stock} initial={1} onAdd={handleAddToCart} />
+          <ItemCount
+            stock={maxStockAvailable}
+            initial={1}
+            onAdd={handleAddToCart}
+          />
         ) : (
           <div>
             <p className="text-success">
-              Adicionado: {addedQuantity} de {stock}
+              {addedQuantity} unidade(s) adicionada(s) ao carrinho!
             </p>
             <button
               className="btn btn-success w-100"
